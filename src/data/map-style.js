a808d73237f0c099394baaabbdc5868a5fe0d8c0
Nathan Griffiths <ngriffiths@postmedia.com>
2019-08-13 10:24:17 -0700

    updated with colorbrewer blue scale

--- src/data/map-style.js
+++ src/data/map-style.js
@@ -13,12 +13,19 @@ export const dataLayer = fromJS({
 			property: 'percentile',
 			stops: [
 				[1, '#A7A9AB'],
-				[2, '#D4DAEA'],
-				[3, '#AFBEDB'],
-				[4, '#829DC7'],
-				[5, '#6D8EBF'],
-				[6, '#3C76B0'],
-				[7, '#0062A3']
+				[2, '#f1eef6'],
+				[3, '#d0d1e6'],
+				[4, '#a6bddb'],
+				[5, '#74a9cf'],
+				[6, '#2b8cbe'],
+				[7, '#045a8d']
+				// [1, '#A7A9AB'],
+				// [2, '#D4DAEA'],
+				// [3, '#AFBEDB'],
+				// [4, '#829DC7'],
+				// [5, '#6D8EBF'],
+				// [6, '#3C76B0'],
+				// [7, '#0062A3']
 			]
 		},
 		'fill-opacity': 0.8,
--- src/data/settings.js
+++ src/data/settings.js
@@ -1,6 +1,16 @@
 const SETTINGS = {
 	dataUrl: './data/choropleth-data-selectpops.geojson',
-	colors: ['#A7A9AB','#D4DAEA','#AFBEDB','#829DC7','#6D8EBF','#3C76B0','#0062A3', 'darkblue'],
+	// colors: ['#A7A9AB','#D4DAEA','#AFBEDB','#829DC7','#6D8EBF','#3C76B0','#0062A3', 'darkblue'],
+	colors: [
+		'#A7A9AB',
+		'#f1eef6',
+		'#d0d1e6',
+		'#a6bddb',
+		'#74a9cf',
+		'#3690c0',
+		'#0570b0',
+		'#034e7b'
+	], // colorbrewer blues
 	thresholds: [0.1, 5, 10, 25, 35, 50, 75], // needs to match the number of items in colours
 	displayPopulations: ['Filipino','Iranian','Indian','Chinese','Korean','Japanese','Central','British Isles','First Nations/Métis/Inuit'],
 	displayPopulations_top15: ['Chinese','English','Scottish','Canadian','Irish','Indian','German','French','Filipino','Ukranian','Italian','Dutch','Polish','Russian','Korean'],
